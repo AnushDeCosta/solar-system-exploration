9f88945884a79335d4e49a39a245f0e3f15e3e9e
Anush De Costa <anush.de.costa@gmail.com>
2025-04-26 13:07:40 +1000

    Added planet.py file
    #Added Planet class with attributes
    1. Created a new Python file named planet.py.
    2. Defined the Planet class with attributes: name, mass, diameter, density, gravity, distance_from_sun, mean_temperature, moon_count, ring_system, and global_magnetic_field.
    3. Added docstrings to the class and the constructor to describe the purpose and parameters clearly.
    4. Followed PEP8 styling standards for readability and clarity.

---
+++ planet.py
@@ -0,0 +1,29 @@
+class Planet:
+    """
+    Represents a planet with its physical and orbital characteristics.
+    """
+    def __init__(self, name, mass, diameter, density, gravity, distance_from_sun, mean_temperature, moon_count, ring_system, global_magnetic_field):
+        """
+        Initialises a Planet with its physical and orbital attributes.
+        :param name: Name of the planet (str)
+        :param mass: Mass of the planet in 10^24 kg (float)
+        :param diameter: Diameter of the planet in kilometres (int)
+        :param density: Density of the planet in kg/m³ (float)
+        :param gravity: Surface gravity in m/s² (float)
+        :param distance_from_sun: Distance from the Sun in 10^6 km (float)
+        :param mean_temperature: Average surface temperature in °C (int)
+        :param moon_count: Number of moons (int)
+        :param ring_system: Presence of ring system (bool)
+        :param global_magnetic_field: Presence of a global magnetic field (bool)
+        """
+        self.name = name
+        self.mass = mass
+        self.diameter = diameter
+        self.density = density
+        self.gravity = gravity
+        self.distance_from_sun = distance_from_sun
+        self.mean_temperature = mean_temperature
+        self.moon_count = moon_count
+        self.ring_system = ring_system
+        self.global_magnetic_field = global_magnetic_field
+
